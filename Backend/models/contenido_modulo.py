8e613778c44bd4dd9a74831ff21d18e5c13fea88
AngelRamirez02 <ar2754072@gmail.com>
2025-11-07 17:38:06 -0600

    Backend: campos de nombre y url_img en contenidoMoulos

--- Backend/models/contenido_modulo.py
+++ Backend/models/contenido_modulo.py
@@ -7,7 +7,9 @@ class ContenidoModulo(Base):
     id = Column(Integer,  primary_key=True, index=True)
     #Llave foranea que apunta a la tabla modulo
     id_modulo = Column(Integer, ForeignKey('modulo.id'), nullable=False)
+    nombre = Column(String(100))
     descripcion = Column(Text)
     recursos = Column(Text)
     resultado_esperado = Column(Text)
+    url_img_letra = Column(Text)
     modulo = relationship("Modulo", back_populates="contenidos")
